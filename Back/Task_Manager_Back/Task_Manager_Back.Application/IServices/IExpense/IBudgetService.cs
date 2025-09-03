b6cc12f2443799a36a557a6c9c0c5962c71fb49e
EXakiToR <andrey.elciscev@gmail.com>
2025-09-03 20:39:46 +0300

    IExpense interfaces

--- Back/Task_Manager_Back/Task_Manager_Back.Application/IServices/IExpense/IBudgetService.cs
+++ Back/Task_Manager_Back/Task_Manager_Back.Application/IServices/IExpense/IBudgetService.cs
@@ -8,5 +8,5 @@ using Task_Manager_Back.Domain.Aggregates.ShoppingAggregate;
 namespace Task_Manager_Back.Application.IServices.IExpense;
 public interface IBudgetService
 {
-    bool IsWithinBudget(Transaction transaction, Decimal budgetLimit);
+    bool IsWithinBudget(Transaction transaction, decimal budgetLimit);
 }
--- Back/Task_Manager_Back/Task_Manager_Back.Application/IServices/IExpense/ITransactionValidationService.cs
+++ Back/Task_Manager_Back/Task_Manager_Back.Application/IServices/IExpense/ITransactionValidationService.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Task_Manager_Back.Application.IServices.IExpense;
 public interface ITransactionValidationService
 {
+    void Validate(Transaction transaction);
 }
