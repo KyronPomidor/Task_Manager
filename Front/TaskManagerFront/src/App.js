e1e9f5a85a093efd7eb327031ed664443d329e9b
Kyron <kirill.2xbo.c@gmail.com>
2025-09-04 13:41:49 +0300

    sff

--- Front/TaskManagerFront/src/App.js
+++ Front/TaskManagerFront/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './arrow-left.svg';
+function Header() {
+  return (
+    <div className='App-header'>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2 style={{ marginLeft: "1vw" }}>Dashboard</h2>
+      <div style={{ position: 'relative', width: '250px' }}>
+        
+        <input
+          type="search"
+          placeholder="Search"
+          style={{
+            width: '100%',
+            padding: '8px 16px 8px 40px',
+            borderRadius: '25px',
+            border: '1px solid #ccc',
+            outline: 'none',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
