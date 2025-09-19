094ec1c4f90f0eb8c7f1a91e1dfc77c1b60c69e9
EXakiToR <andrey.elciscev@gmail.com>
2025-09-19 13:11:34 +0300

    add color to task

---
+++ Back/Task_Manager_Back/Task_Manager_Back.Application/UseCases/TaskUseCases/ChangeTaskColorUseCase.cs
@@ -0,0 +1,24 @@
+﻿using Task_Manager_Back.Application.IRepositories;
+using Task_Manager_Back.Application.Requests.TaskRequests;
+using Task_Manager_Back.Domain.Entities.TaskRelated;
+
+namespace Task_Manager_Back.Application.UseCases.TaskUseCases;
+
+public class ChangeTaskColorUseCase
+{
+    private readonly ITaskRepository _repository;
+
+    public ChangeTaskColorUseCase(ITaskRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task ExecuteAsync(ChangeTaskColorRequest request)
+    {
+        var task = await _repository.GetByIdAsync(request.TaskId) 
+            ?? throw new KeyNotFoundException($"Task with Id '{request.TaskId}' not found.");
+        task.ChangeColor(request.NewColor);
+
+        await _repository.UpdateAsync(task);
+    }
+}
