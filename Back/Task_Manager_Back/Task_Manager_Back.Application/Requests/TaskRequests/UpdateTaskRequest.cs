2586f6b876cfa55f06851c695daec2008dbec0fe
EXakiToR <andrey.elciscev@gmail.com>
2025-09-22 15:41:55 +0300

    save (continue working on update request)

--- Back/Task_Manager_Back/Task_Manager_Back.Application/Requests/TaskRequests/UpdateTaskRequest.cs
+++ Back/Task_Manager_Back/Task_Manager_Back.Application/Requests/TaskRequests/UpdateTaskRequest.cs
@@ -4,11 +4,11 @@ namespace Task_Manager_Back.Application.Requests.TaskRequests;
 public record UpdateTaskRequest(
     Guid TaskId,
     Guid UserId,
-    string? NewTitle,
-    string? NewDescription,
-    Guid? NewStatusId,
-    Guid? NewCategoryId,
-    DateTime? NewDeadline,
-    bool? IsCompleted,
-    bool? IsFailed
+    string NewTitle,
+    string NewDescription,
+    Guid NewStatusId,
+    Guid NewCategoryId,
+    DateTime NewDeadline,
+    bool IsCompleted,
+    bool IsFailed
 ) : IRequest;
