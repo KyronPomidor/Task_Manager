e127d9c7c075052ba2971b20e53fa66d03f5da78
Mihail Pancenco <mihailpancenco@Mihails-MacBook-Air.local>
2025-09-22 18:29:50 +0300

    made it compolable

--- Back/Task_Manager_Back/Task_Manager_Back.Infrastructure/DbContext/AppDbContext.cs
+++ Back/Task_Manager_Back/Task_Manager_Back.Infrastructure/DbContext/AppDbContext.cs
@@ -1,11 +1,12 @@
 using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Task_Manager_Back.Infrastructure.DatabaseEntities;
 
 namespace Task_Manager_Back.Infrastructure.DbContext;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
@@ -14,4 +15,5 @@ public class AppDbContext : IdentityDbContext
 
     public DbSet<DatabaseTaskEntity> Tasks { get; set; } = null!;
     public DbSet<DatabaseTaskCategory> TaskCategories { get; set; } = null!;
+    // register another DbSet properties here
 }
