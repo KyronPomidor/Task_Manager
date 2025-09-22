8e344bd2a6ff2a3a7677a1ce19e75553a6fd5556
EXakiToR <andrey.elciscev@gmail.com>
2025-09-23 01:05:11 +0300

    GetTaskUserCategories

--- Back/Task_Manager_Back/Task_Manager_Back.Application/Handlers/TaskUserCategoryHandlers/GetTaskUserCategoriesHandler.cs
+++ Back/Task_Manager_Back/Task_Manager_Back.Application/Handlers/TaskUserCategoryHandlers/GetTaskUserCategoriesHandler.cs
@@ -1,5 +1,25 @@
-﻿namespace Task_Manager_Back.Application.Handlers.TaskUserCategoryHandlers;
-public class GetTaskUserCategoriesHandler
-{
+﻿using MediatR;
+using System.Collections.Generic;
+using Task_Manager_Back.Application.Requests.TaskUserCategoryRequests;
+using Task_Manager_Back.Application.UseCases.TaskUserCategoryUseCases;
+using Task_Manager_Back.Domain.Entities.TaskCategories;
 
+namespace Task_Manager_Back.Application.Handlers.TaskUserCategoryHandlers;
+
+public class GetTaskUserCategoriesHandler
+    : IRequestHandler<GetTaskUserCategoriesRequest, IReadOnlyList<TaskUserCategory>>
+{
+    private readonly GetTaskUserCategoriesUseCase _useCase;
+
+    public GetTaskUserCategoriesHandler(GetTaskUserCategoriesUseCase useCase)
+    {
+        _useCase = useCase;
+    }
+
+    public async Task<IReadOnlyList<TaskUserCategory>> Handle(
+        GetTaskUserCategoriesRequest request,
+        CancellationToken cancellationToken)
+    {
+        return await _useCase.ExecuteAsync(cancellationToken);
+    }
 }
